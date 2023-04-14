ac703be9aa3d30be5aedea5c4f97f5884ac42948
Julia Ramos <julia.jrra@gmail.com>
2023-04-14 11:33:59 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import * as S from "./assets/components/style";
+import NavBar from "./assets/components/NavBar/Navbar";
+
+
+export default function App(){
+
+  return(
+    <>
+    <S.GlobalStyle/>
+    <NavBar/>
+    </>
+  );
+  
+};
